e019b24bc4ef3b276bdc763f9a825ea12e39a566
Albert Solana <iamtxena@gmail.com>
2026-02-13 23:31:47 +0100

    [C-02] Generate TypeScript SDK from OpenAPI (#92)
    * Add ADR for data module placement and ownership
    
    * [C-01] Freeze OpenAPI v1 contract and add freeze tests
    
    * [C-02] Generate TypeScript SDK from OpenAPI

---
+++ sdk/typescript/src/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BacktestsApi';
+export * from './DeploymentsApi';
+export * from './HealthApi';
+export * from './OrdersApi';
+export * from './PortfoliosApi';
+export * from './ResearchApi';
+export * from './StrategiesApi';
